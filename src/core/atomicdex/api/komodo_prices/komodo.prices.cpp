162750f26133f568a6a3d69fc6dfd8417ed1a1f9
cipig <cipianon@gmail.com>
2021-11-22 09:15:04 +0000

    HTTPS

--- src/core/atomicdex/api/komodo_prices/komodo.prices.cpp
+++ src/core/atomicdex/api/komodo_prices/komodo.prices.cpp
@@ -10,7 +10,7 @@
 
 namespace
 {
-    constexpr const char*                 g_komodo_prices_endpoint = "http://price.cipig.net:1313";
+    constexpr const char*                 g_komodo_prices_endpoint = "https://prices.cipig.net:1717";
     web::http::client::http_client_config g_komodo_prices_cfg{[]()
                                                               {
                                                                   web::http::client::http_client_config cfg;
